58aeddde3acf5afb45e62591f6f75005cf2647ca
MakarTest <mahadrum@gmail.com>
2023-04-28 09:37:45 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ПРивет!");
+        }
+    }
+}
